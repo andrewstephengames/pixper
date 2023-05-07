51f67effb5edf1c5153f62d5fffdd67ff6fdbe73
andrewstephengames <andrewstephengames999@gmail.com>
2023-05-07 19:21:26 +0300

    Add stats using sqlite3 db

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import random
 import math
 import sys
 import os
+import sqlite3
 
 from pygame import mixer
 
 # initialize pygame
 pygame.init ()
-#pygame.mixer.init()
+pygame.mixer.init()
 
 # window stuff
 
@@ -35,11 +36,9 @@ menuTile = pygame.image.load (os.path.normpath(os.path.join("./", "res/images/me
 tinyGrassTile = pygame.image.load (os.path.normpath(os.path.join("./", "res/images/tinyGrasstile.png")))
 bombTile = pygame.image.load (os.path.normpath(os.path.join("./", "res/images/bombtile.png")))
 
-# player
-
+#player
 #playerImg = pygame.image.load(os.path.normpath(os.path.join("./", "res/images/player-black.png")))
 playerImg = pygame.image.load(os.path.normpath(os.path.join("./", "res/images/player-test-sprite.png")))
-enemyImg = pygame.image.load(os.path.normpath(os.path.join("./", "res/images/enemy-test-sprite.png")))
 playerX = random.randint (0, width-32)
 playerY = random.randint (0, height-32)
 playerSpeed = 3
@@ -50,6 +49,7 @@ hitDelay = 20
 
 enemyX = random.randint (0, width-32)
 enemyY = random.randint (0, height-32)
+enemyImg = pygame.image.load(os.path.normpath(os.path.join("./", "res/images/enemy-test-sprite.png")))
 enemySpeed = 1
 
 # obstacles
@@ -58,7 +58,6 @@ appleImg = []
 appleX = []
 appleY = []
 appleNum = 0
-
 
 grassTileImg = []
 grassTileX = []
@@ -74,9 +73,6 @@ treeImg = []
 treeX = []
 treeY = []
 treeNum = 0
-
-# switches
-
 
 # fonts
 
@@ -107,9 +103,68 @@ pygame.mixer.music.play (-1)
 
 keys = {'w': False,'a': False,'s': False,'d': False}
      
-
+# initialize score
 score = 0
+# initialize player name
+playerName = "Player"
 
+musicPaused = False
+
+def toggleMusic():
+    global musicPaused
+    if not musicPaused:
+        pygame.mixer.music.pause()
+        for i in range(100):
+            screen.blit (healthFont.render("Music toggled off!", True, (255, 255, 255)), (0, 0))
+        musicPaused = True
+    else:
+        pygame.mixer.music.unpause()
+        for i in range(100):
+            screen.blit (healthFont.render("Music toggled on!", True, (0, 255, 0)), (0, 0))
+        musicPaused = False
+# stats.db database path
+statsPath= os.path.normpath(os.path.join("./", "res/db/stats.db"))
+
+# command-line arguments
+if len(sys.argv) > 1:
+# toggle the music if the command line argument `mute` is used
+     if sys.argv[1] == "mute":
+          toggleMusic()
+          if len(sys.argv) > 2:
+               if sys.argv[2] == "cleardb":
+                    with open(statsPath, "w") as file:
+                         file.truncate()
+# clear the database if the command line argument `cleardb` is used
+     if sys.argv[1] == "cleardb":
+          with open(statsPath, "w") as file:
+               file.truncate()
+          if len(sys.argv) > 2:
+               if sys.argv[2] == "mute":
+                    toggleMusic()
+
+
+# initialize stats-keeping database
+conn = sqlite3.connect(os.path.normpath(os.path.join("./", "res/db/stats.db")))
+c = conn.cursor()
+c.execute(
+          '''CREATE TABLE IF NOT EXISTS Players
+          (
+               Name TEXT PRIMARY KEY,
+               Score INTEGER,
+               Hard BOOLEAN
+          )
+          '''
+         )
+c.execute(
+          '''CREATE TABLE IF NOT EXISTS Obstacles
+          (
+               Playername TEXT,
+               RNG REAL PRIMARY KEY,
+               Name TEXT,
+               FOREIGN KEY(Playername) REFERENCES Players(Name)
+          )
+          '''
+         )
 # entities
 
 def player(x, y):
@@ -134,14 +189,16 @@ def toggleDifficulty():
           randX = 16
           randY = 64
           enemySpeed = 2
-          screen.blit (healthFont.render("Hard Mode Enabled!", True, (255, 0, 0)), (0, 0))
+          for i in range(100):
+               screen.blit (healthFont.render("Hard Mode Enabled!", True, (255, 0, 0)), (0, 0))
           print("Hard Mode Enabled!")
      else:
           hardMode = False
           randX = 8
           randY = 32
           enemySpeed = 1
-          screen.blit (healthFont.render("Hard Mode Disabled!", True, (255, 255, 255)), (0, 0))
+          for i in range(100):
+               screen.blit (healthFont.render("Hard Mode Disabled!", True, (255, 255, 255)), (0, 0))
           print("Hard Mode Disabled!")
      
 def isCollision (x1, y1, x2, y2, collide):
@@ -151,10 +208,10 @@ def isCollision (x1, y1, x2, y2, collide):
 def generateApple (init):
      global appleImg, appleX, appleY, appleNum, tinyGrassTile
      global playerX, playerY, playerSpeed, score, width, height, playerHealth
-     global randX, randY
+     global randX, randY, randAppleX, randAppleY, playerName, conn, c
      if hardMode:
           randX = 16
-          randY = 32
+          randY = 64
      else:
           randX = 8
           randY = 32
@@ -162,8 +219,10 @@ def generateApple (init):
           appleNum = random.randint (randX, randY)
           for i in range (appleNum):
                appleImg.append (pygame.image.load(os.path.normpath(os.path.join ("./", "res/images/apple.png"))))
-               appleX.append(random.randint (32, width-32))
-               appleY.append(random.randint (32, height-32))
+               randAppleX = random.randint(32, width-32)
+               randAppleY = random.randint(32, height-32)
+               appleX.append(randAppleX)
+               appleY.append(randAppleY)
      else:
           for i in range (appleNum):
                screen.blit (appleImg[i], (appleX[i], appleY[i]))
@@ -171,6 +230,8 @@ def generateApple (init):
                     if appleImg[i] != tinyGrassTile:
                          eatSound.play()
                          eatSound.set_volume(0.1)
+                         RNG = (appleX[i]+appleY[i])/2
+                         c.execute("INSERT OR REPLACE INTO Obstacles VALUES (?, ?, ?)", (playerName, RNG, "Apple"))
                          score += 1
                          playerSpeed += 0.25
                          screen.blit (tinyGrassTile, (appleX[i], appleY[i]))
@@ -191,9 +252,9 @@ def generateGrass (init):
 def generateBomb (init):
      global bombNum, bombImg, bombX, bombY, width, height, playerHealth
      global enemyX, enemyY, playerX, playerY, playerSpeed, enemySpeed
-     global bombTile, randX, randY
+     global bombTile, randX, randY, c, conn, playerName, enemyName
      if hardMode:
-          randX = 32
+          randX = 48
           randY = 96
      else:
           randX = 8
@@ -217,6 +278,8 @@ def generateBomb (init):
                          playerHealth -= 10
                          playerSpeed -= 0.5
                          screen.blit (bombTile, (bombX[i], bombY[i]))
+                         RNG = (bombX[i]+bombY[i])/2
+                         c.execute("INSERT OR REPLACE INTO Obstacles VALUES (?, ?, ?)", (playerName, RNG, "Bomb"))
                          bombImg[i] = bombTile
                          bombSound.play()
                          bombSound.set_volume(0.1)
@@ -227,14 +290,17 @@ def generateBomb (init):
                if isCollision (enemyX, enemyY, bombX[i], bombY[i], 30):
                     if bombImg[i] != bombTile:
                          enemySpeed += 0.5
+                         RNG = (bombX[i]+bombY[i])/2
+                         playerName = "Enemy"
+                         c.execute("INSERT OR REPLACE INTO Obstacles VALUES (?, ?, ?)", (playerName, RNG, "Bomb"))
                          screen.blit (bombTile, (bombX[i], bombY[i]))
                          bombImg[i] = bombTile
                          bombSound.play()
                          bombSound.set_volume(0.1)
                     else:
                          if playerX == bombX[i] and playerY == bombY[i]:
-                              playerX -= 1
-                              playerY -= 1
+                              playerX -= 5
+                              playerY -= 5
                               playerSpeed -= 0.05
 
 def generateTree (init):
@@ -259,16 +325,6 @@ def placeTile(tileImg):
                j += 256
           i += 256
 
-musicPaused = False
-
-def toggleMusic():
-    global musicPaused
-    if not musicPaused:
-        pygame.mixer.music.pause()
-        musicPaused = True
-    else:
-        pygame.mixer.music.unpause()
-        musicPaused = False
 
 def titleBlit():
      global width, height, screen
@@ -280,22 +336,41 @@ def titleBlit():
      screen.blit (titleFont.render("E", True, (0, 0, 255)), (width/8+400, heightModifier))
      screen.blit (titleFont.render("R", True, (106, 13, 173)), (width/8+500, heightModifier))
 
+# initialize the menus
 
-#menu = pygame_menu.Menu('Pixper', width, height, theme=pygame_menu.themes.THEME_SOLARIZED)
 menu = pygame_menu.Menu('Pixper', width, height, theme=pygame_menu.themes.THEME_DARK)
-#menu = pygame_menu.Menu('Pixper', width, height, theme=pygame_menu.themes.THEME_BLUE)
+menu2 = pygame_menu.Menu('Stats', width, height, theme=pygame_menu.themes.THEME_DARK)
 
-# toggle the music if the command line argument mute is used
-if len(sys.argv) > 1 and sys.argv[1] == "mute":
-     toggleMusic()
 
 def startGame():
     global menu
     gameLoop()
     menu.disable()
 
+def storePlayer(name):
+    global playerName
+    playerName = name
+
+def printStats():
+     global screen, c
+     c.execute("SELECT * FROM Players")
+     print (c.fetchall())
+     #c.execute("SELECT * FROM Obstacles")
+     #print(c.fetchall())
+
+def statsMenu():
+     global width, height, screen, menu2
+     menu2.clear()
+     c.execute ('SELECT Name, Score, Hard FROM Players ORDER BY Score DESC')
+     scores = c.fetchall()
+     menu2.add.button("Name | Score | Hard", printStats)
+     for i in scores:
+          menu2.add.button(f'{i[0]} | {i[1]} | {i[2]}', printStats)
+     menu2.add.button ('Back', pygame_menu.events.BACK)
+     menu2.mainloop(screen)
+
 def mainMenu ():
-     global width, height, menuTile, screen
+     global width, height, menuTile, screen, menu, menu2, c
      resize = False
      running = True
      heightModifier = height/8
@@ -325,16 +400,26 @@ def mainMenu ():
                     running = False
 #          screen.blit (bgImg, (0, 0))
           menu.font = titleFont
-          menu.add.text_input('Name: ', default='Player')
-          #menu.add.selector('Difficulty: ',  [('Easy', 1), ('Hard', 2)], onchange=setDifficulty)
+          menu.add.text_input('Name: ', default='Player', onchange=storePlayer)
           menu.add.button('Toggle Difficulty', toggleDifficulty)
-          playButton = menu.add.button('Play', startGame)
+          menu.add.button('Play', startGame)
+          menu.add.button('Stats', menu2)
           menu.add.button('Quit', pygame_menu.events.EXIT)
           menu.add.button('Toggle Music', toggleMusic)
-
+          c.execute ('SELECT Playername, RNG, Name FROM Obstacles ORDER BY RNG DESC LIMIT 3')
+          obstacles = c.fetchall()
+          c.execute ('SELECT Name, Score, Hard FROM Players ORDER BY Score DESC LIMIT 3')
+          scores = c.fetchall()
+          menu2.add.button ('Back', pygame_menu.events.BACK)
+          menu2.add.button("Name | Score | Hard", printStats)
+          for i in scores:
+               menu2.add.button(f'{i[0]} | {i[1]} | {i[2]}', printStats)
+          menu2.add.button("                         ", printStats)
+          menu2.add.button("Playername | RNG | Name", printStats)
+          for i in obstacles:
+               menu2.add.button(f'{i[0]} | {i[1]} | {i[2]}', printStats)
           if menu.is_enabled():
                menu.mainloop (screen)
-
           clock = pygame.time.Clock()
           clock.tick (75)
           pygame.display.update()
@@ -350,7 +435,7 @@ def gameLoop():
      iterationNum = 0
      global width, height, grassTile, screen
      global playerX, playerY, enemyX, enemyY, playerSpeed, enemySpeed
-     global playerHealth, hitDelay, menuTile
+     global playerHealth, hitDelay, menuTile, playerName, c
      running = True
      while running:
           iterationNum += 1
@@ -439,8 +524,7 @@ def gameLoop():
           if enemyY >= height-32:
                enemyY = height-32
      
-          
-          collision = isCollision (playerX, playerY, enemyX, enemyY, 2)
+          collision = isCollision (playerX, playerY, enemyX, enemyY, 5)
           if collision and hitDelay == 0:
                hurtSound.play()
                hurtSound.set_volume(0.1)
@@ -453,10 +537,15 @@ def gameLoop():
                playerHealth = 0
                screen.blit (endFont.render ("Game Over!", True, (163.6, 162.5, 162.5)), (width/10, height/2.5))
                screen.blit (endFont.render ("Score:" + str(score), True, (0, 0, 255)), (width/5, height/2))
+               playerName = "Enemy"
+               c.execute("INSERT OR REPLACE INTO Players VALUES (?, ?, ?)", (playerName, score, hardMode))
+               conn.commit()
                iterationNum = 0
           elif score == appleNum:
                screen.blit (endFont.render ("You Won!", True, (223.8, 225.7, 12.1)), (width/5, height/2.5))
                screen.blit (endFont.render ("Score:" + str(score), True, (0, 0, 255)), (width/5, height/2))
+               c.execute("INSERT OR REPLACE INTO Players VALUES (?, ?, ?)", (playerName, score, hardMode))
+               conn.commit()
                iterationNum = 0
           
           player(playerX, playerY)
@@ -471,10 +560,21 @@ def gameLoop():
           if iterationNum == 0:
                #pygame.time.wait(2500)
                running = False
+               print("Player: ", playerName)
+               printStats()
                pygame_menu.events.EXIT
 mainMenu()
-if score == 0:
-     print ("You ate no apples.")
-elif score == 1:
-     print ("You ate", score, "apple.")
-else: print ("You ate", score, "apples.")
+c.close()
+conn.close()
+if playerName != "Enemy":
+     if score == 0:
+          print ("You ate no apples.")
+     elif score == 1:
+          print ("You ate", score, "apple.")
+     else: print ("You ate", score, "apples.")
+else:
+     if score == 0:
+          print ("The Enemy stole no apples.")
+     elif score == 1:
+          print ("The Enemy stole", score, "apple.")
+     else: print ("The Enemy stole", score, "apples.")
